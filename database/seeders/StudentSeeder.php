51963decdd0604f4d86e84529bb1ef1f9d04d854
FaizaNA07 <faizaabida44614@gmail.com>
2021-10-18 22:48:42 -0700

    lastweek

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -13,26 +13,6 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('students')->insert([
-            'nim' => '2031710068',
-            'name' => 'Faiza Nur Abida',
-            'class' => 'MI 2F',
-            'department' => 'JTI-MI',
-            'phone_number' => '0858-5395-7585',
-        ]);
-        DB::table('students')->insert([
-            'nim' => '2031710177',
-            'name' => 'Ghulam Al Fakhri',
-            'class' => 'MI 2F',
-            'department' => 'JTI-MI',
-            'phone_number' => '0857-4556-9538',
-        ]);
-        DB::table('students')->insert([
-            'nim' => '2031710034',
-            'name' => 'Jihan Rahadatul Aisy',
-            'class' => 'MI 2F',
-            'department' => 'JTI-MI',
-            'phone_number' => '0813-8425-6211',
-        ]);
+      //
     }
 }
